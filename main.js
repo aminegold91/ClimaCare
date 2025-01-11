20eb14aa137300a674e4212731a0a45c1b4b8255
Lamine el kouss <aminegold91@gmail.com>
2025-01-11 11:28:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.getElementById("bookingForm").addEventListener("submit", function(event) {
+/*document.getElementById("bookingForm").addEventListener("submit", function(event) {
       event.preventDefault();
   
       // جلب البيانات من النموذج
@@ -29,3 +29,22 @@ document.getElementById("bookingForm").addEventListener("submit", function(event
               console.error(error);
           });
   });
+  */
+
+
+function send(){
+  let nember  = '+212767576688';
+let name = document.getElementById("name").value;
+let phone = document.getElementById("phone").value;
+let address = document.getElementById("address").value;
+let type = document.getElementById("type").value;
+let problem = document.getElementById("problem").value;
+let date = document.getElementById("date").value;
+var url = 'https://wa.me/' + nember + '?text=' + ' الاسم : ' + name + '%0a'
++ 'رقم الهاتف  : ' + phone + '%0a'
++ 'العنوان : ' + address + '%0a'
++ 'النوع : ' + type + '%0a'
++ 'وصف المشكلة :' + problem + '%0a%0a'
++ 'التاريخ : ' + date + '%0a';
+window.open(url , '_blanck').focus();
+}
