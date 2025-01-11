865c59b2afd560a2621670272be48ca7d3283c10
Lamine el kouss <aminegold91@gmail.com>
2025-01-11 11:47:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+
+
+function send(){
+let nember  = '+212767576688';
+let name = document.getElementById("name").value;
+let phone = document.getElementById("phone").value;
+let address = document.getElementById("address").value;
+let type = document.getElementById("type").value;
+let problem = document.getElementById("problem").value;
+let date = document.getElementById("date").value;
+var url = 'https://wa.me/' + nember + '?text=' + ' الاسم : ' + name + '%0a'
++ 'رقم الهاتف  : ' + phone + '%0a'
++ 'العنوان : ' + address + '%0a'
++ 'النوع : ' + type + '%0a'
++ 'وصف المشكلة :' + problem + '%0a%0a'
++ 'التاريخ : ' + date + '%0a';
+window.open(url , '_blanck').focus();
+}
+
+
+
 /*document.getElementById("bookingForm").addEventListener("submit", function(event) {
       event.preventDefault();
   
@@ -32,19 +53,3 @@
   */
 
 
-function send(){
-  let nember  = '+212767576688';
-let name = document.getElementById("name").value;
-let phone = document.getElementById("phone").value;
-let address = document.getElementById("address").value;
-let type = document.getElementById("type").value;
-let problem = document.getElementById("problem").value;
-let date = document.getElementById("date").value;
-var url = 'https://wa.me/' + nember + '?text=' + ' الاسم : ' + name + '%0a'
-+ 'رقم الهاتف  : ' + phone + '%0a'
-+ 'العنوان : ' + address + '%0a'
-+ 'النوع : ' + type + '%0a'
-+ 'وصف المشكلة :' + problem + '%0a%0a'
-+ 'التاريخ : ' + date + '%0a';
-window.open(url , '_blanck').focus();
-}
